6a966528621351f260d511e62620abd189c9616e
Ivan_Budayeu <Skileton2018>
2022-05-11 15:00:40 +0300

    Dependencies exposing update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("java-library")
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.springframework.boot") version "2.6.7"
 }
@@ -13,9 +14,10 @@ subprojects {
     group = "com.budaev.caching"
     version = "1.0-SNAPSHOT"
 
-    apply(plugin = "java");
-    apply(plugin = "org.springframework.boot");
-    apply(plugin = "io.spring.dependency-management");
+    apply(plugin = "java")
+    apply(plugin = "java-library")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
 
     java.sourceCompatibility = JavaVersion.VERSION_11
     java.targetCompatibility = JavaVersion.VERSION_11
@@ -25,7 +27,6 @@ subprojects {
     }
 
     dependencies {
-        implementation("org.springframework.boot:spring-boot-starter-web")
         testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
         testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
     }
--- web-common/build.gradle.kts
+++ web-common/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    api("org.springframework.boot:spring-boot-starter-web")
 }
 
 tasks.getByName<Task>("bootJar").enabled = false
